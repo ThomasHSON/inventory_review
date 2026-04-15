f0456150582115252f0d665b23497d1e1e88430e
ThomasHSON <hson_thomas@kutech.tw>
2026-04-15 15:51:17 +0800

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
   return (
     <footer className="fixed bottom-0 w-full bg-white shadow-lg bg-white border-t border-gray-200 z-20">
-      <div className="max-w-7xl mx-auto px-1 sm:px-1 lg:px-2 py-1">
+      <div className="px-1 sm:px-1 lg:px-2 py-1">
         <p className="text-sm text-center text-gray-600">{t('copyright')}</p>
       </div>
     </footer>
