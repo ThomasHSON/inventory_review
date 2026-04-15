44521701fe2664fc6de93018015c72839360070d
ThomasHSON <hson_thomas@kutech.tw>
2026-04-15 15:51:34 +0800

    Updated Tabs.tsx

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -6,7 +6,7 @@ const Tabs = () => {
 
   return (
     <div className="h-[40px] mb-2">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex space-x-8 border-b border-gray-200">
           <a href="../inventory_manager" className="px-4 py-2 text-base font-medium border-b-2 border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300">
             {t('tabs.manage')}
