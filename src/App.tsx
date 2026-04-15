dccb3d923f03550fe6021db8fbb070e07a2527d9
ThomasHSON <hson_thomas@kutech.tw>
2026-04-15 15:51:00 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -495,7 +495,7 @@ function App() {
       <Navbar user={user} onLogout={handleLogout} />
       <Tabs />
 
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 mb-10">
+      <main className="px-4 sm:px-6 lg:px-8 py-8 mb-10">
         <ButtonSection
           onInventorySettings={() => setIsInventoryModalOpen(true)}
           onUnitPriceSettings={() => setIsUnitPriceModalOpen(true)}
